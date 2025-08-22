77953396c85dc7646160eeb44648aa2c1b7a7ede
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-22 14:27:16 -0300

    Método byscar_contato finalizado

--- gerenciador_contatos.rb
+++ gerenciador_contatos.rb
@@ -3,14 +3,30 @@
 # criando array agenda para armazernar os contatos
 agenda = []
 
-# criando hash contato 
+# criando método para adicionar contatos 
 def adiciona_contato (lista, nome, telefone)
     contato = {nome: nome, telefone: telefone}
     lista.push(contato)
 end
 
+# criando método para buscar contato
+def buscar_contatos (lista, nome)
+    lista.each do |key, value|
+        if value == nome
+            puts(lista.nome)
+        else
+            puts("Nome não existe em contatos")
+            break
+        end
+    end
+
+end
+
 adiciona_contato(agenda, "tibas", 93220367)
 adiciona_contato(agenda, "maria", 93939393)
 adiciona_contato(agenda, "tomy", 31133223)
+
+buscar_contatos(agenda, "tibas")
+buscar_contatos(agenda, "joao")
 
 puts(agenda)
