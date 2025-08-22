e0ef01e5831f1efaf5701503c1dc9cc7d304bab9
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-22 17:37:47 -0300

    Adicionando e melhorando comentários

--- gerenciador_contatos.rb
+++ gerenciador_contatos.rb
@@ -5,12 +5,14 @@ agenda = []
 
 # criando método para adicionar contatos 
 def adiciona_contato (lista, nome, telefone)
+    # Criando contato como símbolo pois, eles são imutáveis e reutilizados, eles são mais rápidos para comparação e acesso em hashes.
     contato = {nome: nome, telefone: telefone}
     lista.push(contato)
 end
 
 # criando método para buscar contato
 def buscar_contatos (lista, nome_busca)
+    # O método .find {|item| condição}, percorre o array/hash testa cada elemento é retorna o primeiro item que faz a condição ser verdadeira, nesse caso o que ele faz é procurar por um contato que bate com o nome de busca
     contato = lista.find { |c| c[:nome] == nome_busca }
     if contato
         puts("#{contato[:nome]} - #{contato[:telefone]}")
@@ -21,6 +23,7 @@ end
 
 # criando método de imprimir agenda
 def mostrar_agenda (lista)
+    # O método each pecorre cada elemento da agenda e imprime cada um
     lista.each do |contato|
         puts("#{contato[:nome]} - #{contato[:telefone]}")
     end
@@ -28,14 +31,18 @@ end
 
 # criando método excluir contatos
 def excluir_contato (lista, contato_excluir)
+    # O método .find percorre toda a agenda até achar o contato a ser excluido
     contato = lista.find { |c| c[:nome] == contato_excluir }
     if contato
+        # O método .delete excluir o contato
         lista.delete(contato)
     else
         puts("Nome não existe em contatos")
     end
 end
 
+=begin
+#testando código    
 adiciona_contato(agenda, "tibas", 93220367)
 adiciona_contato(agenda, "maria", 93939393)
 adiciona_contato(agenda, "tomy", 31133223)
@@ -46,6 +53,8 @@ buscar_contatos(agenda, "joao")
 mostrar_agenda(agenda)
 excluir_contato(agenda, "tibas")
 mostrar_agenda(agenda)
+
+=end
 
 # recebendo dados do usuário
 loop do
@@ -71,7 +80,4 @@ loop do
         puts("Saindo...")
         break
     end
-    
 end
-
-puts(agenda)
