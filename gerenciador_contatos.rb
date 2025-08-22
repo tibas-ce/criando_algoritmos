a622b01d557015a8769af42c1625eae8d6def3b4
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-22 16:43:35 -0300

    Método de excluir contato finalizado

--- gerenciador_contatos.rb
+++ gerenciador_contatos.rb
@@ -26,6 +26,16 @@ def mostrar_agenda (lista)
     end
 end
 
+# criando método excluir contatos
+def excluir_contato (lista, contato_excluir)
+    contato = lista.find { |c| c[:nome] == contato_excluir }
+    if contato
+        lista.delete(contato)
+    else
+        puts("Nome não existe em contatos")
+    end
+end
+
 adiciona_contato(agenda, "tibas", 93220367)
 adiciona_contato(agenda, "maria", 93939393)
 adiciona_contato(agenda, "tomy", 31133223)
@@ -34,23 +44,29 @@ buscar_contatos(agenda, "tibas")
 buscar_contatos(agenda, "joao")
 
 mostrar_agenda(agenda)
+excluir_contato(agenda, "tibas")
+mostrar_agenda(agenda)
 
 # recebendo dados do usuário
 loop do
-    puts("Olá, bem vindo ao nosso gerenciador de contatos.\n O que você gostaria de fazer?\n Cadastrar um novo contato? Dígite c\n Buscar um contato? Dígite b\n para sair dígite s")
+    puts("Olá, bem vindo ao nosso gerenciador de contatos.\n O que você gostaria de fazer?\n Cadastrar um novo contato? Dígite c\n Buscar um contato? Dígite b\n Quer excluir um contato? Dígite e\n Para mostar agenda dígite m\n Para sair dígite s")
     resposta = gets.chomp.downcase
     if resposta == "c"
         puts("Dígite o nome do contato")
         nome = gets.chomp
         puts("Dígite o telefone de #{nome}")
         telefone = gets.chomp
-        adiciona_contato(agenda, nome, telefone)
-        
+        adiciona_contato(agenda, nome, telefone)        
     elsif resposta == "b"
         puts("Dígite o nome do contato que você quer buscar")
         nome_buscar = gets.chomp
         buscar_contatos(agenda, nome_buscar)
-        
+    elsif resposta == "e"
+        puts("Dígite o nome do contato que você quer exluir")
+        nome_excluir = gets.chomp
+        excluir_contato(agenda, nome_excluir)
+    elsif resposta == "m"
+        mostrar_agenda(agenda)
     else
         puts("Saindo...")
         break
