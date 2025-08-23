5116d4802d05bfc8fa96877d5b0e6c14905fc98e
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-23 17:25:23 -0300

    Adicionando método de calcular vendas

--- analisador_vendas.rb
+++ analisador_vendas.rb
@@ -23,7 +23,7 @@ class Vendas
     end
 
     # método calcular valor total de vendas
-    def calcular_vendas ()
+    def calcular_vendas 
         soma = 0.0
         @vendas.each do |venda|
             soma += venda[:valor] 
@@ -33,7 +33,9 @@ class Vendas
 
     # método encontrar maior venda
     def maior_venda 
+        # O .first e usado para pegar o primeiro elemento de um array
         maior = @vendas.first
+        # no caso do método maior_venda precisamos de um ponto de partida para comparar, o .each pecorre o array, o if compara, e maior muda de acordo com o resultado da comparação
         @vendas.each do |venda|
             if maior[:valor] < venda[:valor]
                 maior = venda
@@ -42,15 +44,25 @@ class Vendas
         maior
     end
 
+    # método de calcular media de vendas
+    def calcular_media 
+        soma = calcular_vendas
+        # O .size é usado para obter o número de elementos dentro de um objeto que possui uma coleção, como uma string, array ou hash. Nesse caso o array @vendas
+        media = soma / @vendas.size
+        return media
+    end
+
     def mostrar_lista
         soma = calcular_vendas
         resultado_maior_venda = maior_venda
+        media = calcular_media
         # O método each pecorre cada elemento das vendas e imprime cada um
         @vendas.each do |venda|
             puts("#{venda[:produto]} - #{venda[:valor]}")
         end
         puts("Valor total de vendas = #{soma}")
         puts("Maior venda foi #{resultado_maior_venda[:produto]} com valor de #{resultado_maior_venda[:valor]}")
+        puts("A média de vendas é #{media}")
     end
 end
 
