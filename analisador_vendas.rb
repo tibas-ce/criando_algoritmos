83356b72384f02d0afbeabda160ae9a7c9661b23
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-25 12:35:33 -0300

    Adicionando interações com o usuário

--- analisador_vendas.rb
+++ analisador_vendas.rb
@@ -64,19 +64,27 @@ class Vendas
         contador
     end
 
-    def mostrar_lista
+    def mostrar_lista (resposta)
+        # variáveis chamando os métodos
         soma = calcular_vendas
         resultado_maior_venda = maior_venda
         media = calcular_media
         vendas_acima = contar_vendas
-        # O método each pecorre cada elemento das vendas e imprime cada um
-        @vendas.each do |venda|
-            puts("#{venda[:produto]} - #{venda[:valor]}")
+        # verificação da respota do usuário
+        if resposta == "v"
+            # O método each pecorre cada elemento das vendas e imprime cada um
+            @vendas.each do |venda|
+                puts("#{venda[:produto]} - #{venda[:valor]}")
+            end
+        elsif resposta == "t"  
+            puts("Valor total de vendas = #{soma}")
+        elsif resposta == "m"
+            puts("Maior venda foi #{resultado_maior_venda[:produto]} com valor de #{resultado_maior_venda[:valor]}")
+        elsif resposta == "e"    
+            puts("A média de vendas é #{media}")
+        elsif resposta == "a"
+            puts("A quantidade de vendas acima de R$ 200,00 é #{vendas_acima}")
         end
-        puts("Valor total de vendas = #{soma}")
-        puts("Maior venda foi #{resultado_maior_venda[:produto]} com valor de #{resultado_maior_venda[:valor]}")
-        puts("A média de vendas é #{media}")
-        puts("A quantidade de vendas acima de R$ 200,00 é #{vendas_acima}")
     end
 end
 
@@ -88,4 +96,30 @@ vendas.add_venda("Mouse", 150.00)
 vendas.add_venda("Monitor", 750.00)
 vendas.add_venda("Notebook", 2500.00)
 
-vendas.mostrar_lista
+#vendas.mostrar_lista
+
+# interações co  o usuário
+loop do
+    puts("Olá, bem vindo ao nosso analisador de vendas!\n O que você gostaria de fazer?\n Cadastrar uma nova venda dígite: c\n Ver lista de vendas dígite v\n Quer ver o valor total das vendas dígite t\n Ver maior venda dígite m\n Ver média de vendas dígite e\n Ver vendas acima de R$200,00 dígite a\n Dígite s para sair.")
+    resposta = gets.chomp.downcase
+    if resposta == "c"
+        puts("Dígite o nome do produto:")
+        produto = gets.chomp
+        puts("Dígite o valor do produto:")       
+        valor = gets.chomp.to_f
+        vendas.add_venda(produto, valor)
+    elsif resposta == "v"
+        vendas.mostrar_lista(resposta)
+    elsif resposta == "t"
+        vendas.mostrar_lista(resposta)
+    elsif resposta == "m"
+        vendas.mostrar_lista(resposta)
+    elsif resposta == "e"
+        vendas.mostrar_lista(resposta)
+    elsif resposta == "a"
+        vendas.mostrar_lista(resposta)
+    else
+        puts("Saindo")
+        break
+    end
+end
